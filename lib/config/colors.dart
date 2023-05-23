fa330a88cd2c654320eb1d74da32447ed193b633
ScionRs <grazs116@gmail.com>
2023-05-23 14:01:37 +0300

    Added colors to app

---
+++ lib/config/colors.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color black1 = Color.fromRGBO(1, 8, 39, 1);
+  static const Color black2 = Color.fromRGBO(26, 30, 64, 1);
+  static const Color black3 = Color.fromRGBO(54, 54, 77, 1);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1);
+  static const Color success = Color.fromRGBO(54, 54, 77, 1);
+  static const Color warning = Color.fromRGBO(255, 165, 0, 1);
+  static const Color error = Color.fromRGBO(223, 80, 96, 1);
+
+}
