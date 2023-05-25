b7c036eaa6b49eee4da3c591522a4b7ee18d40be
ScionRs <grazs116@gmail.com>
2023-05-25 12:07:17 +0300

    Changed succes color

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -2,12 +2,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors{
-  static const Color black1 = Color.fromRGBO(1, 8, 39, 1);
-  static const Color black2 = Color.fromRGBO(26, 30, 64, 1);
-  static const Color black3 = Color.fromRGBO(54, 54, 77, 1);
-  static const Color white = Color.fromRGBO(255, 255, 255, 1);
-  static const Color success = Color.fromRGBO(54, 54, 77, 1);
-  static const Color warning = Color.fromRGBO(255, 165, 0, 1);
-  static const Color error = Color.fromRGBO(223, 80, 96, 1);
+  static const black1 = Color.fromRGBO(1, 8, 39, 1);
+  static const black2 = Color.fromRGBO(26, 30, 64, 1);
+  static const black3 = Color.fromRGBO(54, 54, 77, 1);
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+  static const success = Color.fromRGBO(64, 199, 0, 1);
+  static const warning = Color.fromRGBO(255, 165, 0, 1);
+  static const error = Color.fromRGBO(223, 80, 96, 1);
 
 }
